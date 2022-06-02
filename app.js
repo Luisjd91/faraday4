0f3f1948d3939783af087dde290c12fafe462f4d
Luisjd91 <106349905+Luisjd91@users.noreply.github.com>
2022-06-02 16:01:38 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const Edaddelusuario= parseInt(prompt("Cual es tu edad?"));
+if (Edaddelusuario>=18){
+	form.style.display="flex"
+	alert ("Eres mayor de edad, complete el formulario y solicite su turno")
+} else{
+	alert ("Eres menor de edad, no puedes solicitar turnos")
+	 }
+
